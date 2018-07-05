c30c31139321c7dfbf4ab0f2153b81ade0e89206
AlexanderSierraPulido <40802531+AlexanderSierraPulido@users.noreply.github.com>
2018-07-05 16:52:40 +0000

    Add files via upload

---
+++ SierraAlexander_S5C3.cpp
@@ -0,0 +1,41 @@
+// Hola Mundo
+cout << "Hello";
+
+// multiplicar cpp
+int a = 17;
+int b = 31;
+int array [0];
+
+int main () 
+{ 
+}
+
+
+int Multiplicacion(int a, int b)
+	{
+	c = a*b;
+	}
+cout << "El valor de la multiplicacion entre 17 y 31 es", c ;
+int Cuadrado(int a)
+	{
+	d = a*a;	
+	}
+cout << "El valor de 17 elevado al cuadrado es", d;
+
+// numeros.cpp
+
+int * numeros()
+	{
+	for (i, i<89, i++)
+		{
+		if (i%2 = 0)
+			{
+			array = array + i
+			}
+		else if (array.length() == 25)
+			{
+			break
+			}
+		}
+	}
+
